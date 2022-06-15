2775b35dbba8dc4b584cd23c18b7bdfce7435093
Orfox <77915094+orfox32@users.noreply.github.com>
2022-06-15 19:38:06 +0800

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,80 +1,40 @@
-var WIDTH;
-var HEIGHT;
-var canvas;
-var con;
-var g;
-var pxs = new Array();
-var rint = 50;
+var total=40,
+  container=document.getElementById('container'),
+  w=window.innerWidth,
+  h=window.innerHeight,
+  Tweens=[],
+  SPs=1;
 
-$(document).ready(function(){
-  WIDTH = window.innerWidth;
-  HEIGHT = window.innerHeight;
-	$('#container').width(WIDTH).height(HEIGHT);
-	canvas = document.getElementById('pixie');
-	$(canvas).attr('width', WIDTH).attr('height',HEIGHT);
-	con = canvas.getContext('2d');
-	for(var i = 0; i < 50; i++) {
-		pxs[i] = new Circle();
-		pxs[i].reset();
-	}
-	setInterval(draw,rint);
-	setInterval(draw,rint2);
 
-});
 
-function draw() {
-	con.clearRect(0,0,WIDTH,HEIGHT);
-	for(var i = 0; i < pxs.length; i++) {
-		pxs[i].fade();
-		pxs[i].move();
-		pxs[i].draw();
-	}
-}
+for (var i=total;i--;){ 
+  var Div=document.createElement('div');
+  TweenLite.set(Div,{attr:{class:'dot'},x:R(w),y:R(h),opacity:0});
+  container.appendChild(Div); Anim(Div);  Tweens.push(Div);
+};
 
-function Circle() {
-	this.s = {ttl:8000, xmax:5, ymax:2, rmax:10, rt:1, xdef:960, ydef:540, xdrift:4, ydrift: 4, random:true, blink:true};
+function Anim(elm){ 
+  elm.Tween=TweenLite.to(elm,R(20)+10,{bezier:{values:[{x:R(w),y:R(h)},{x:R(w),y:R(h)}]},opacity:R(1),scale:R(1)+0.5,delay:R(2),onComplete:Anim,onCompleteParams:[elm]})
+};
 
-	this.reset = function() {
-		this.x = (this.s.random ? WIDTH*Math.random() : this.s.xdef);
-		this.y = (this.s.random ? HEIGHT*Math.random() : this.s.ydef);
-		this.r = ((this.s.rmax-1)*Math.random()) + 1;
-		this.dx = (Math.random()*this.s.xmax) * (Math.random() < .5 ? -1 : 1);
-		this.dy = (Math.random()*this.s.ymax) * (Math.random() < .5 ? -1 : 1);
-		this.hl = (this.s.ttl/rint)*(this.r/this.s.rmax);
-		this.rt = Math.random()*this.hl;
-		this.s.rt = Math.random()+1;
-		this.stop = Math.random()*.2+.4;
-		this.s.xdrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
-		this.s.ydrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
-	}
+for(var i=total;i--;){
+  Tweens[i].Tween.play()};
 
-	this.fade = function() {
-		this.rt += this.s.rt;
-	}
+window.addEventListener("resize", resize);
 
-	this.draw = function() {
-		if(this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt*-1;
-		else if(this.rt >= this.hl) this.reset();
-		var newo = 1-(this.rt/this.hl);
-		con.beginPath();
-		con.arc(this.x,this.y,this.r,0,Math.PI*2,true);
-		con.closePath();
-		var cr = this.r*newo;
-		g = con.createRadialGradient(this.x,this.y,0,this.x,this.y,(cr <= 0 ? 1 : cr));
-		g.addColorStop(0.0, 'rgba(238,180,28,'+newo+')');
-		g.addColorStop(this.stop, 'rgba(238,180,28,'+(newo*.2)+')');
-		g.addColorStop(1.0, 'rgba(238,180,28,0)');
-		con.fillStyle = g;
-		con.fill();
-	}
+function R(max){return Math.random()*max};
 
-	this.move = function() {
-		this.x += (this.rt/this.hl)*this.dx;
-		this.y += (this.rt/this.hl)*this.dy;
-		if(this.x > WIDTH || this.x < 0) this.dx *= -1;
-		if(this.y > HEIGHT || this.y < 0) this.dy *= -1;
-	}
+function resize() {
+  
+  w = window.innerWidth;
+  h = window.innerHeight;
+  
+  for (var i = 0; i < total; i++) {
+    
+    var fireFly = Tweens[i];
 
-	this.getX = function() { return this.x; }
-	this.getY = function() { return this.y; }
+    TweenLite.killTweensOf(fireFly);
+    
+    Anim(fireFly);
+  }
 }
